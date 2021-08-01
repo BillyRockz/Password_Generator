c3d342bbef5c521eeeed2bc530f9993afa31bb19
GuillePr <guillermopr95@gmail.com>
2021-08-01 13:59:07 -0500

    Files
    Adding files

---
+++ Password_Generator.py
@@ -0,0 +1,9 @@
+dict_alpha = {"A":2, "B":1, "C":3, "D":7,"E":7, "F":0, "G":6, "H":4, "I":3, "J":5, "K":7, "L":5, "M":0, "N":9, "O":1, "P":6, "Q":1, "R":4, "S":6, "T":6, "U":8, "V":5, "W":4, "X":2, "Y":8, "Z":2}
+
+def turn_into_numbers(password):
+  number_pass = []
+  for letter in password:
+    number_pass.append(dict_alpha.get(letter))
+  return number_pass
+
+print(turn_into_numbers("BANK"))
